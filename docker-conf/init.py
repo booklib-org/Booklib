6fafb1891351e30d3e12ea3f98f4bb6b9e977d76
Harvey Barnes <ldshadowlord@gmail.com>
2021-09-17 15:52:24 +0100

    Added more verbose output

--- docker-conf/init.py
+++ docker-conf/init.py
@@ -3,6 +3,8 @@ print("Starting Container...")
 #Initial import
 import os
 import subprocess
+print("Imports Succesful")
 
 # Supervisord takes it from here.
 nginx = subprocess.Popen(["/usr/bin/supervisord", "-c", "/etc/supervisor/conf.d/supervisord.conf"])
+print("Supervisord has Authority...")
