8428632a410967a822dea7de55deb311187f6093
Harvey Barnes <ldshadowlord@gmail.com>
2021-09-17 19:20:24 +0100

    Updated Logic to keep container alive

--- docker-conf/init.py
+++ docker-conf/init.py
@@ -22,7 +22,7 @@ ENV_FILE = {
 }
 
 # Initial import
-print("Starting Imports")
+print("Starting Imports...")
 import os
 import subprocess
 import sys
@@ -34,11 +34,16 @@ try:
 
 # Try to pull Environment variables
 except:
+    print("Searching for Environmental Variables...")
     for item in ENV_FILE.keys():
         try:
-            ENV_FILE[item] = os.getenv(item)
+            e_v = os.getenv(item)
+            if e_v == None:
+                raise OSError("ENV Variable not set")
+            else:
+                ENV_FILE[item] = os.getenv(item)
         except:
-            print(item+" was not set, setting to default.")
+            print(item+" was not set, using default.")
             # If it's a required item that is missing, shut down the server and fail.
             if item in ["DB_HOST", "DB_DATABASE", "DB_USERNAME", "DB_PASSWORD"]:
                 print("Cannot continue. "+item+" is a required variable and MUST BE SET.")
@@ -47,8 +52,10 @@ except:
     ENV_FILE_LIST = []
     for item in ENV_FILE.keys():
         ENV_FILE_LIST.append(item+"="+str(ENV_FILE[item]))
+    print("Writing new .env file and forming SymLink")
     with open('/storage/.env', 'w') as file:
-        file.writelines( ENV_FILE_LIST )
+        for line in ENV_FILE_LIST:
+            file.write(line+"\n\n")
     os.system("ln -s /storage/.env /Booklib/.env")
 
 # If we've never seen the user before, run initial DB key generation
@@ -59,7 +66,7 @@ try:
     InitS.close()
 except:
     print("This container has not been run before, doing initial key generation")
-    result = subprocess.run("cd /Booklib && php artisan key:generate")
+    result = subprocess.run("cd /Booklib && php artisan key:generate", shell=True)
     if result.returncode != 0:
         print("Encountered error during Key Generation")
         print("Exit code for Key Generation was "+str(result.returncode))
@@ -71,12 +78,30 @@ except:
 
 # If everything else has passed, run php artisan commands
 print("Running Artisan")
-result = subprocess.run("cd /Booklib && php artisan migrate")
-print("Exit code for Migration was "+str(result.returncode))
-result = subprocess.run("cd /Booklib && php artisan db:seed")
-print("Exit code for seeding was "+str(result.returncode))
+result = subprocess.run("cd /Booklib && php artisan migrate --force", shell=True, capture_output=True)
+if result.returncode != 0:
+    print("Encountered error during Migration")
+    print("Exit code for Migration was "+str(result.returncode))
+    print(result.stderr)
+    print("----")
+    print(result.stdout)
+    print("----")
+else:
+    print("Migration Successful")
+
+result = subprocess.run("cd /Booklib && php artisan db:seed --force", shell=True, capture_output=True)
+if result.returncode != 0:
+    print("Encountered error during seeding")
+    print("Exit code for seeding was "+str(result.returncode))
+    print(result.stderr)
+    print("----")
+    print(result.stdout)
+    print("----")
+else:
+    print("Seeding Successful")
 
 # Finally, launch supervisord and start the server.
 print("Launching Supervisord...")
-svd = subprocess.Popen(["/usr/bin/supervisord" "-c" "/etc/supervisor/conf.d/supervisord.conf"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-stdout, stderr = svd.communicate()
+svd = subprocess.Popen(["/usr/bin/supervisord " " -c " " /etc/supervisor/conf.d/supervisord.conf"],shell=True)
+while True:
+    pass
