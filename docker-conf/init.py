f196609391a69d13e5e31e80638fbba85dc0857f
Harvey Barnes <ldshadowlord@gmail.com>
2021-09-17 15:49:00 +0100

    Added initial logic

--- docker-conf/init.py
+++ docker-conf/init.py
@@ -1,1 +1,8 @@
-#!/usr/local/binpy
+print("Starting Container...")
+
+#Initial import
+import os
+import subprocess
+
+# Supervisord takes it from here.
+nginx = subprocess.Popen(["/usr/bin/supervisord", "-c", "/etc/supervisor/conf.d/supervisord.conf"])
