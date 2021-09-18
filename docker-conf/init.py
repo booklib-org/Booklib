21f2ba527d4eb34ebb613ce178ea33bb5d129ab3
Harvey Barnes <ldshadowlord@gmail.com>
2021-09-19 00:07:53 +0100

    Added proper logging + code cleaning

--- docker-conf/init.py
+++ docker-conf/init.py
@@ -1,5 +1,21 @@
 #!/usr/local/bin/python3
-print("Starting Container...")
+# Initial import
+import os
+import subprocess
+import sys
+import logging
+
+# Configure Logging
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    handlers=[
+        logging.StreamHandler(sys.stdout),
+        logging.FileHandler(filename="/storage/logs/init.log", encoding="utf-8")
+    ]
+)
+
+logging.info("Starting Container...")
 ENV_FILE = {
     "APP_NAME":"BookLib",
     "APP_ENV":"local",
@@ -21,20 +37,14 @@ ENV_FILE = {
     "SESSION_LIFETIME":"120"
 }
 
-# Initial import
-print("Starting Imports...")
-import os
-import subprocess
-import sys
-
 # Check for existing .env file
 try:
     env_file_lines = open("/Booklib/.env", "r").readlines()
-    print("Existing .env file discovered. Will not change settings.")
+    logging.warning("Existing .env file discovered. Will not change settings.")
 
 # Try to pull Environment variables
 except:
-    print("Searching for Environmental Variables...")
+    logging.info("Searching for Environmental Variables...")
     for item in ENV_FILE.keys():
         try:
             e_v = os.getenv(item)
@@ -43,16 +53,16 @@ except:
             else:
                 ENV_FILE[item] = os.getenv(item)
         except:
-            print(item+" was not set, using default.")
+            logging.warning(item+" was not set, using default.")
             # If it's a required item that is missing, shut down the server and fail.
             if item in ["DB_HOST", "DB_DATABASE", "DB_USERNAME", "DB_PASSWORD"]:
-                print("Cannot continue. "+item+" is a required variable and MUST BE SET.")
+                logging.error("Cannot continue. "+item+" is a required variable and MUST BE SET.")
                 os.system("poweroff")
                 sys.exit()
     ENV_FILE_LIST = []
     for item in ENV_FILE.keys():
         ENV_FILE_LIST.append(item+"="+str(ENV_FILE[item]))
-    print("Writing new .env file and forming SymLink")
+    logging.info("Writing new .env file and forming SymLink")
     with open('/storage/.env', 'w') as file:
         for line in ENV_FILE_LIST:
             file.write(line+"\n")
@@ -62,59 +72,55 @@ except:
 # We don't know if this fails
 try:
     InitS = open("/storage/Init_Success", "r")
-    print("This container has been run before, skipping key generation")
+    logging.info("This container has been run before, skipping key generation")
     InitS.close()
 except:
-    print("This container has not been run before, doing initial key generation")
+    logging.info("This container has not been run before, doing initial key generation")
     result = subprocess.run("cd /Booklib && php artisan key:generate", shell=True)
     if result.returncode != 0:
-        print("Encountered error during Key Generation")
-        print("Exit code for Key Generation was "+str(result.returncode))
+        logging.error("Encountered error ["+str(result.returncode)+"]during Key Generation")
     else:
-        print("Key Generation Successful")
+        logging.info("Key Generation Successful")
         InitS = open("/storage/Init_Success", "w")
         InitS.write("Key Generated")
         InitS.close()
 
 # If everything else has passed, run php artisan commands
-print("Running Artisan")
+logging.info("Running Artisan")
 result = subprocess.run("cd /Booklib && php artisan migrate --force", shell=True, capture_output=True)
 if result.returncode != 0:
-    print("Encountered error during Migration")
-    print("Exit code for Migration was "+str(result.returncode))
-    print(result.stderr)
-    print("----")
-    print(result.stdout)
-    print("----")
+    logging.error("Encountered error ["+str(result.returncode)+"]during Migration")
+    logging.warning(result.stderr)
+    logging.warning("----")
+    logging.warning(result.stdout)
+    logging.warning("----")
 else:
-    print("Migration Successful")
+    logging.info("Migration Successful")
 
 result = subprocess.run("cd /Booklib && php artisan db:seed --force", shell=True, capture_output=True)
 if result.returncode != 0:
-    print("Encountered error during seeding")
-    print("Exit code for seeding was "+str(result.returncode))
-    print(result.stderr)
-    print("----")
-    print(result.stdout)
-    print("----")
+    logging.error("Encountered error ["+str(result.returncode)+"]during Seeding")
+    logging.warning(result.stderr)
+    logging.warning("----")
+    logging.warning(result.stdout)
+    logging.warning("----")
 else:
-    print("Seeding Successful")
+    logging.info("Seeding Successful")
 
 # Run a Git Pull to make sure it's up to date
-print("Checking for updates....")
+logging.info("Checking for updates....")
 result = subprocess.run("cd /Booklib && git pull https://github.com/LDShadowLord/Booklib.git", shell=True, capture_output=True)
 if result.returncode != 0:
-    print("Encountered error during update")
-    print("Exit code for update was "+str(result.returncode))
-    print(result.stderr)
-    print("----")
-    print(result.stdout)
-    print("----")
+    logging.error("Encountered error ["+str(result.returncode)+"]during Update")
+    logging.warninging(result.stderr)
+    logging.warning("----")
+    logging.warning(result.stdout)
+    logging.warning("----")
 else:
-    print("Update Successful")
+    logging.warning("Update Successful or no Update Required")
 
 # Finally, launch supervisord and start the server.
-print("Launching Supervisord...")
+logging.info("Launching Supervisord...")
 svd = subprocess.Popen(["/usr/bin/supervisord " " -c " " /etc/supervisor/conf.d/supervisord.conf"],shell=True)
 while True:
     pass
