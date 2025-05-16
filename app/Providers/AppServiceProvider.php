de9e9c2f91689969d4eb2a249201a88f6df78fe3
Martijn Katerbarg <martijnkaterbarg@gmail.com>
2025-05-16 16:47:41 +0200

    Set HTTPS handler

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
